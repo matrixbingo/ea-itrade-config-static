0dda5d2d5f5432fb9c0f8776ee52f2772852892f
Matrix <87679214@qq.com>
2017-05-21 21:30:07 +0800

    ad

---
+++ src/view/pages/config.jsx
@@ -0,0 +1,35 @@
+import React, { Component /*,PropTypes*/} from 'react'
+import { Redirect, Router, Route } from 'react-router'
+import {page} from 'gfs-react-dm'
+import History from 'history/lib/createHashHistory'
+import TestContainer from './Test'
+import Index from '../components/test/Index'
+
+class AppConfigRouter extends Component {
+
+    constructor(props) {
+        super(props)
+        // Opt-out of persistent state, not recommended.
+        this.history = new History({
+            queryKey: false
+        })
+    }
+
+    /**
+     * 页面路由总览，children为外接做入口，外接入口即为AppRouter
+     */
+    render() {
+        return (
+            <div>
+                <Router history={this.history}>
+                    <Route path="/index" component={Index} />
+                    <Route path="/test" component={TestContainer} />
+                    <Route path="/qwe" component={TestContainer} />
+                    <Redirect from="/" to="/index" />
+                </Router>
+            </div>
+        )
+    }
+}
+
+page(AppConfigRouter )
